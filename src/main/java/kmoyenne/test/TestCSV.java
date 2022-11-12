16aa3f748c727513c520984353e6930b6b25d877
bryanbucket <bryanlim@hotmail.ca>
2022-11-12 03:11:11 -0500

    fix imports + add test

--- src/main/java/kmoyenne/test/TestCSV.java
+++ src/main/java/kmoyenne/test/TestCSV.java
@@ -3,16 +3,20 @@ package kmoyenne.test;
 import kmoyenne.Barycentre;
 import kmoyenne.Point;
 import kmoyenne.utils.CsvUtils;
+import org.junit.Test;
 
 import java.util.List;
 
+import static org.junit.Assert.assertEquals;
+
 public class TestCSV {
 
-    public static void main(String[] args) {
+    @Test
+    public void testCSV() throws Exception {
         List<Barycentre> barycentres = CsvUtils.extractBarycentres();
         System.out.println(barycentres.toString());
 
         List<Point> data = CsvUtils.extractData();
-        System.out.println(data.toString());
+        assertEquals("[largeur : 40 prix : 60, largeur : 30 prix : 60, largeur : 20 prix : 60, largeur : 10 prix : 60, largeur : 0 prix : 60]",data.toString());
     }
 }
